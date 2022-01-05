35f57dda270ce24ee19292d2b3f935324babb52d
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-01-05 18:40:34 +0900

    [add] first commit, 회원도메인

---
+++ src/main/java/com/springcore/member/Grade.java
@@ -0,0 +1,5 @@
+package com.springcore.member;
+
+public enum Grade {
+    BASIC, VIP
+}
---
+++ src/main/java/com/springcore/member/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.springcore.member;
+
+public interface MemberRepository {
+
+    void save(Member member);
+
+    Member findById(Long memberId);
+}

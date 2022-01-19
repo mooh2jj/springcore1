37a0a9546561a17efa3e78b5c74823c3d3585397
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-01-19 15:24:11 +0900

    [add] 프록시로 가짜 MyLogger 만들기

--- src/main/java/com/springcore/web/LogDemoController.java
+++ src/main/java/com/springcore/web/LogDemoController.java
@@ -14,15 +14,16 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    //    private final MyLogger myLogger;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+        private final MyLogger myLogger;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("/log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        MyLogger myLogger = myLoggerProvider.getObject();
+//        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.log("controller test");
 
         logDemoService.logic("testId");
